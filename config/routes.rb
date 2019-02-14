bc54ab79751ef52071b89f127dac135c6fe0e602
Chris Ragasa <tofuragasa@gmail.com>
2019-02-14 00:32:18 -0600

    Generated a controller for search results.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'search/results'
   resources :pages
   resources :teams, only: %i[index show]
   resources :coaches, only: %i[index show]
