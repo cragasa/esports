7d36e4097e92e6a9489a82c6e1df14a8e048ef67
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 22:44:32 -0600

    Generated a controller for players.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'players/index'
+  get 'players/show'
   resources :teams, only: %i[index show]
   resources :coaches, only: %i[index show]
 
