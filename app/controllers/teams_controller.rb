7e2381b3c035038c9bde240efcff2b23efa868d1
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 22:13:24 -0600

    Created a controller for teams, modified routes to default to the teams index, index displays all teams, when a team is clicked it will navigate to a page displaying the team with their players and coaches.

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 class TeamsController < ApplicationController
   def index
+    @teams = Team.order(:name)
   end
 
   def show
+    @team = Team.includes(:players, :coaches).find(params[:id])
+    # @team = Team.find(21)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'teams/index'
-  get 'teams/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :teams, only: %i[index show]
+
+  root to: 'teams#index'
 end
