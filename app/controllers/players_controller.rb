33dc505311f54eca34a57b0a39bfe8324cb6b7cb
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 22:51:03 -0600

    Modified the controller to have and index page which displays all players along with their team, after a player is clicked you will be navigated to a show page which displays the players team size and name.

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class PlayersController < ApplicationController
   def index
+    @players = Player.order(:name)
   end
 
   def show
+    @player = Player.find(params[:id])
   end
 end
