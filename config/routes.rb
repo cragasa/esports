e1d03d70a7a7037a62ffbb7b8e65f0ff2fa463e1
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 23:31:37 -0600

    Scaffolded Pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :pages
   resources :teams, only: %i[index show]
   resources :coaches, only: %i[index show]
   resources :players, only: %i[index show]
