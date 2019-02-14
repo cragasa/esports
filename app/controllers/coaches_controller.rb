9d5757e4b8f1125ad3ca6b91ce24a5b269a4667c
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 22:24:39 -0600

    Generated a coaches controller.

---
+++ app/controllers/coaches_controller.rb
@@ -0,0 +1,7 @@
+class CoachesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'coaches/index'
+  get 'coaches/show'
   resources :teams, only: %i[index show]
 
   root to: 'teams#index'
