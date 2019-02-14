ee3f8a6cfcfee2852d9c991d60c56a05f093181f
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 22:45:37 -0600

    Modified routes for restful routing of players.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'players/index'
-  get 'players/show'
   resources :teams, only: %i[index show]
   resources :coaches, only: %i[index show]
+  resources :players, only: %i[index show]
 
   root to: 'teams#index'
 end
