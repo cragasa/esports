73c7ff097fe964fa53758e4d9871a1e2b8d1bbab
Chris Ragasa <tofuragasa@gmail.com>
2019-02-14 01:02:09 -0600

    Added a search function that will query through Teams, Players, and Coaches.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
+
 class SearchController < ApplicationController
   def results
+    @query = params[:q]
+    @teams = Team.where('name LIKE ?', "%#{@query}%")
+    @players = Player.where('name LIKE ?', "%#{@query}%")
+    @coaches = Coach.where('name LIKE ?', "%#{@query}%")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'search/results'
   resources :pages
   resources :teams, only: %i[index show]
   resources :coaches, only: %i[index show]
   resources :players, only: %i[index show]
 
   get 'static/:permalink', to: 'pages#static', as: 'static'
-
+  get 'search_results', to: 'search#results', as: 'search_results'
   root to: 'teams#index'
 end
