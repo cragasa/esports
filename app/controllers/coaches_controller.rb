805179ed74770b33ba5b93214aeeb79ff3bc72f6
Chris Ragasa <tofuragasa@gmail.com>
2019-02-13 22:42:22 -0600

    Created route for coaches, page will display all coaches with their team, once a coach is clicked a show page will display the coaches team and the amount of coaches in that team.

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class CoachesController < ApplicationController
   def index
+    @coaches = Coach.order(:name)
   end
 
   def show
+    @coach = Coach.find(params[:id])
   end
 end
